70b78c3dd2f6f97bc3f173fc06b062f4a1dfee2b
Your Name <you@example.com>
2026-06-30 23:14:19 -0500

    Card Hawk v1.6.0 - JSON import pipeline

---
+++ cardhawk/importers/__init__.py
@@ -0,0 +1,11 @@
+"""
+Card Hawk Importers
+
+Version 1.6.0
+"""
+
+from .json_importer import JSONImporter
+
+__all__ = [
+    "JSONImporter",
+]
---
+++ cardhawk/importers/json_importer.py
@@ -0,0 +1,31 @@
+"""
+JSON Importer
+
+Version 1.6.0
+"""
+
+from __future__ import annotations
+
+import json
+
+from cardhawk.asset_vault import Asset
+
+
+class JSONImporter:
+
+    def load(self, filename):
+
+        with open(filename, "r", encoding="utf-8") as f:
+
+            records = json.load(f)
+
+        assets = []
+
+        for record in records:
+
+            assets.append(
+                Asset(**record)
+            )
+
+        return assets
+
